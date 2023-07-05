be4274475905f448afd9df2cb1b95a16658377e5
Julia Fox <134555725+juliafox6@users.noreply.github.com>
2023-07-05 16:31:54 +0300

    Delete Form3 directory

--- Form3/Form3/task_din.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-// Заголовочный файл task_din.h
-using namespace System;
-using namespace System::Windows::Forms;
-
-int* input(int&, TextBox^);
-void output(int*, int, ListBox^);
-int* task(int*, int, int&);
